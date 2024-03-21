f85ba62087d431f1765bec3f137189c9f2d262fa
RonexLemon <ronexondimu@gmail.com>
2024-03-21 23:11:21 +0300

    time:done time

--- Goroutines/timee/timee.go
+++ Goroutines/timee/timee.go
@@ -15,5 +15,10 @@ func  Time(){
 	//format time
 	fmt.Println(presentTime.Format("01-02-2006 15:04:05 Monday")) //always the format
 
+	createDate:= time.Date(2020, time.January,10,10,15,30,40, time.UTC)
+
+	fmt.Println(createDate)
+	fmt.Println(createDate.Format("01-02-2006 15:04:05 Monday"))
+
 	
 }
