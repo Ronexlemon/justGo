bd0d191630d2cb5dc5b3d6a5fbb16387ac3765e3
RonexLemon <ronexondimu@gmail.com>
2024-03-31 14:23:16 +0300

    remove:item at index?

--- Goroutines/Slices/slice.go
+++ Goroutines/Slices/slice.go
@@ -29,6 +29,15 @@ func Slices(){
 	sort.Ints(highScores)
 	fmt.Println(highScores)
 	fmt.Println(sort.IntsAreSorted(highScores)) // returns true if sorted
+
+	//remove value based on index
+
+	var courses  = []string{"reactjs","javascript","python","ruby"}
+
+	fmt.Println(courses)
+	var index int = 2
+	courses = append(courses[:index],courses[index+1:]...) //skip that index
+	fmt.Println(courses)
 	
 
 
