a10149ac546ba581894172637a2f3e216b1b7ea7
RonexLemon <ronexondimu@gmail.com>
2024-04-01 05:42:35 +0300

    for:range

--- Goroutines/controlflows/loops.go
+++ Goroutines/controlflows/loops.go
@@ -9,9 +9,12 @@ func Loops(){
 	days := []string{"sunday","Tuesday","wednesday","friday","saturday"}
 	fmt.Println(days)
 
-	////forloop
-	for d:=0; d< len(days);d++{
-		fmt.Println("days ", days[d])
+	// ////forloop
+	// for d:=0; d< len(days);d++{
+	// 	fmt.Println("days ", days[d])
+	// }
+	//for range
+	for key,value := range days{
+		fmt.Printf("the key is %d and value %s\n",key,value)
 	}
-	
 }
