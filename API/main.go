df5c0aef13e8ad094760cebfee7512d493f3a5af
RonexLemon <ronexondimu@gmail.com>
2024-04-23 21:30:36 +0300

    create data

--- API/main.go
+++ API/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -29,7 +32,8 @@ var courses []Course
 //helper/middleware functions
 func (c *Course) IsEmpty()bool{
 
-	return c.CourseId == "" && c.CourseName == ""
+	// return c.CourseId == "" && c.CourseName == ""
+	return c.CourseId == ""
 
 
 }
@@ -81,3 +85,42 @@ func getOneCourse(w http.ResponseWriter,r *http.Request){
 	return
 }
 
+
+func createOneCourse(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Create one course")
+	w.Header().Set("Content-Type","application/json")
+
+	//what if body is empty
+
+	if(r.Body) == nil{
+		json.NewEncoder(w).Encode("Please Send some data")
+		
+	}
+
+	//what about the data that is sent like {}
+
+	var course Course
+
+	_=json.NewDecoder(r.Body).Decode(&course)
+
+	if course.IsEmpty(){
+		json.NewEncoder(w).Encode("No data inside JSON")
+		return
+	}
+	//generate unique id
+
+
+	rand.Seed(time.Now().UnixNano())
+
+	course.CourseId = strconv.Itoa(rand.Intn(100))
+
+	courses = append(courses, course)
+
+	json.NewEncoder(w).Encode(course)
+
+	return
+
+	
+
+	
+}
