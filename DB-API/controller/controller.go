bbb3abccf8427cf76bd80d3a28e6ab0e8a2dd13a
RonexLemon <ronexondimu@gmail.com>
2024-04-26 08:29:40 +0300

    chore:connect to db

---
+++ DB-API/controller/controller.go
@@ -0,0 +1,43 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const connectionString = " mongodb+srv://huge65702:huge65701@cluster0.9j3wobi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //just for test
+
+const dbName = "netflix"
+const colName = "watvhlist"
+
+var collection *mongo.Collection
+
+//connect with mongodb
+
+func init() {
+//client options
+clientOption:= options.Client().ApplyURI(connectionString)
+
+//connect to db
+
+client,err:=mongo.Connect(context.TODO(),clientOption)
+checkNil(err)
+fmt.Println("mongodb connection succesful")
+
+//create collection
+collection = client.Database(dbName).Collection(colName)
+//for collection check
+fmt.Println("collection instance is ready yollow!!")
+
+
+}
+
+func checkNil(err error){
+	if err !=nil{
+		log.Fatal(err)
+	}
+}
