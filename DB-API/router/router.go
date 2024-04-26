9b61b2267646a93ce73183bfeb4251aed982b124
RonexLemon <ronexondimu@gmail.com>
2024-04-26 16:07:59 +0300

    feat:add delete a movie router

--- DB-API/router/router.go
+++ DB-API/router/router.go
@@ -17,7 +17,7 @@ func Router()*mux.Router{
 	router.HandleFunc("/api/deleteAll",controller.DeleteAllMovies).Methods("DELETE")
 	router.HandleFunc("/api/movie",controller.CreateMovie).Methods("POST")
 	router.HandleFunc("/api/movie/{id}",controller.MarkMovieAsWatched).Methods("PUT")
-	router.HandleFunc("/api/movie",controller.CreateMovie).Methods("DELETE")
+	router.HandleFunc("/api/movie",controller.DeleteAMovie).Methods("DELETE")
 
 	return router
 }
