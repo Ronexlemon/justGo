a6eaa3eb17107366af15f84dae84afa7c1ac6044
RonexLemon <ronexondimu@gmail.com>
2024-04-29 12:47:04 +0300

    chore:mux get data

--- Mux/main.go
+++ Mux/main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"gomux/router"
+	"log"
+	"net/http"
 )
 func main(){
 	welcome:="Go Mux "
 	fmt.Println(welcome)
+
+	log.Fatal(http.ListenAndServe(":4000",router.Router()))
 }
