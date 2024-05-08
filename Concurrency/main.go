52910adfa795ad3b72e8b80bb04f6e9ecc632013
RonexLemon <ronexondimu@gmail.com>
2024-05-08 06:03:09 +0300

    feat:get Status code

--- Concurrency/main.go
+++ Concurrency/main.go
@@ -2,19 +2,33 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
+	// "time"
 )
 
 func main(){
 	welcome:="Welcome to go concurrency"
 	fmt.Println(welcome)
-	go greeter("Hello")
-	 greeter("Yollow")
+	getStatusCode("https://github.com/RonexLemon")
+	
 }
 
-func greeter(s string){
-	for i:=0; i< 6;i++{
-		time.Sleep(3 * time.Millisecond)
-		fmt.Println(s)
+// func greeter(s string){
+// 	for i:=0; i< 6;i++{
+// 		time.Sleep(3 * time.Millisecond)
+// 		fmt.Println(s)
+// 	}
+// }
+
+func getStatusCode(endpoint string){
+	res,err:=http.Get(endpoint)
+	checkNill(err)
+
+	fmt.Println(res.Status)
+}
+
+func checkNill(err error){
+	if err!=nil{
+		panic(err)
 	}
 }
