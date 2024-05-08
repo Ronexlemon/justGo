5bbbbbb7e87973be575af0fccb35c9f5f54f6168
RonexLemon <ronexondimu@gmail.com>
2024-05-08 05:55:46 +0300

    wait for a thread

--- Concurrency/main.go
+++ Concurrency/main.go
@@ -1,9 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main(){
 	welcome:="Welcome to go concurrency"
-
 	fmt.Println(welcome)
+	go greeter("Hello")
+	 greeter("Yollow")
+}
+
+func greeter(s string){
+	for i:=0; i< 6;i++{
+		time.Sleep(3 * time.Millisecond)
+		fmt.Println(s)
+	}
 }
