103fcfba4889bb62cc43f0c81425ed51cafe8bcd
RonexLemon <ronexondimu@gmail.com>
2024-05-11 10:26:51 +0300

    init channels

---
+++ Channels/channels/channels.go
@@ -0,0 +1,15 @@
+package channels
+
+
+type A struct{
+   age	 int 
+	name string
+	
+}
+
+
+
+func Channels(){
+	dataChan := make(chan A)
+
+}
---
+++ Channels/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+
+func main(){
+	welcome := "Yollow welcome to golang channels"
+	fmt.Println(welcome)
+}
