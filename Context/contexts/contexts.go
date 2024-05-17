5d3dca99d0ce4152938b687f242e6e2f331b990c
RonexLemon <ronexondimu@gmail.com>
2024-05-17 07:43:40 +0300

    feat:context

--- Context/contexts/contexts.go
+++ Context/contexts/contexts.go
@@ -1,7 +1,41 @@
 package contexts
 
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"time"
+)
+
+
+
+func callApi(ctx context.Context, userId int)(bool,error){
+	time.Sleep(400 *time.Millisecond)
+
+	if ctx.Err() == context.DeadlineExceeded{
+		return false, errors.New("context time out exceeded")
+	}
+
+	return true, nil
+}
 
 func Contexts(){
 	fmt.Println("Starting")
+ctx,cancel := context.WithTimeout(context.Background(),500*time.Millisecond)
+
+defer cancel()
+userId:=4000
+result,err := callApi(ctx,userId)
+checkNil(err)
+fmt.Println(result)
+
 	
+}
+
+func checkNil(err error){
+	if err !=nil{
+		//panic(err)
+		log.Fatalf("error is %s",err)
+	}
 }
--- Context/main.go
+++ Context/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"gocontext/contexts"
+)
 
 func main(){
 	welcome:="Yollow here we go again Context"
 	fmt.Println(welcome)
 	cont:="Context are a way of adding cancelation or timeout"
 	fmt.Println(cont)
+	contexts.Contexts()
 }
