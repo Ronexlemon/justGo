cf703b129cc95f8d84d131ce7b26d7d6cd013fbe
RonexLemon <ronexondimu@gmail.com>
2024-06-24 06:27:10 +0300

    method with pointer

--- Methods/methods/methods.go
+++ Methods/methods/methods.go
@@ -11,8 +11,14 @@ type Rectangle struct{
 func (r *Rectangle) Square()float64{
 	return r.width * r.height
 }
+func (r *Rectangle) Double(f float64){
+	r.width *= f
+	r.height *= f
+}
 
 func Rec(){
 	r:= Rectangle{10,5}
+	r.Double(10)
+	
 	fmt.Println(r.Square())
 }
