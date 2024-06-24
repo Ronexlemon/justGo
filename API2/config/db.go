e5c26298c6f3764cc88ad20e42164ff99ebcca9d
RonexLemon <ronexondimu@gmail.com>
2024-06-24 09:06:38 +0300

    done data layer

--- API2/config/db.go
+++ API2/config/db.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"context"
+	"fmt"
 	"golangapis/model"
 
 	"go.mongodb.org/mongo-driver/bson"
@@ -32,6 +33,47 @@ func (r *EmployeeRepo) FindEmployeeById(empId string)(*model.Employee,error){
 		return &emp,nil
 }
 
+func (r *EmployeeRepo) FindAllEmployees()([]model.Employee,error){
+	results,err := r.MongoCollection.Find(context.Background(),bson.D{})
+	if err !=nil{
+		return nil,err
+	}
+
+	var emps []model.Employee
+
+	err = results.All(context.Background(),&emps)
+	if err != nil {
+		return nil, fmt.Errorf("results decode error %s",err.Error())
+	}
+	return emps,nil
+	
+}
+
+func (r *EmployeeRepo) UpdateEmployeeById(empid string, updateEmp *model.Employee)(int64,error){
+	result,err:= r.MongoCollection.UpdateByID(context.Background(),bson.D{{Key: "employee_id", Value: empid}},bson.D{{Key: "$set",Value: updateEmp}})
+	if err != nil {
+		return 0,err
+		}
+		return result.ModifiedCount,nil
+}
+
+func (r *EmployeeRepo) DeleteEmployeeById(empId string)(int64, error){
+
+	result,err := r.MongoCollection.DeleteOne(context.Background(),bson.D{{Key: "employee_id", Value: empId}})
+	if err != nil {
+		return 0,err
+		}
+		return result.DeletedCount,nil
+}
+
+func (r *EmployeeRepo) DeleteAllEmployee()(int64, error){
+
+	result,err := r.MongoCollection.DeleteOne(context.Background(),bson.D{})
+	if err != nil {
+		return 0,err
+		}
+		return result.DeletedCount,nil
+}
 
 func CheckNil(err error){
 	if err != nil {
