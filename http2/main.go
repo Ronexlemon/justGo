4060974440f3bd66015f5361f6da0f58308f1f95
RonexLemon <ronexondimu@gmail.com>
2024-07-30 15:32:02 +0300

    get function

--- http2/main.go
+++ http2/main.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	
 )
 
 func main(){
+	GetFunc()
 
+	
+
+}
+
+
+func GetFunc(){
 	res, err := http.Get("https://github.com/RonexLemon")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	defer res.Body.Close()
 
-	fmt.Println(res)
+	body, _ := io.ReadAll(res.Body)
 
+	fmt.Println(body)
 }
-
