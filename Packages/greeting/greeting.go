f7e0b8e77d48d895142fc482fdd607f475a8a4dd
RonexLemon <ronexondimu@gmail.com>
2024-08-19 12:14:26 +0300

    test

--- Packages/greeting/greeting.go
+++ Packages/greeting/greeting.go
@@ -1,13 +1,16 @@
 package greeting
 
-import "errors"
+import (
+	
+	"fmt"
+)
 
-
+var ErroName  = fmt.Errorf("the first number should be greater than the second")
 func SubTraction(num int, num2 int)(int,error){
 	//the first number should be greater than the second
 
 	if num < num2{
-		return 0,errors.New("the first number should be greater than the second")
+		return 0,ErroName
 	}
 	return (num - num2), nil
 
--- Packages/greeting/greeting_test.go
+++ Packages/greeting/greeting_test.go
@@ -1,18 +1,42 @@
 package greeting
 
-import "testing"
+import (
+	"testing"
+)
 
-func Test_If_FirstNumber_is_Greater_Than_the_Second(t *testing.T) {
+func Test_If_FirstNumber_is_Not_Greater_Than_the_Second(t *testing.T) {
+	var a int = 10
+	var b int = 20
+
+	result, err := SubTraction(a, b)
+	if err == nil {
+		t.Fatalf("Expected an error, but got nil")
+	}
+
+	expectedError := ErroName
+	if err != expectedError {
+		t.Fatalf("Expected error %v, but got %v", expectedError, err)
+	}
+
+	// Check that the result is zero when there is an error
+	if result != 0 {
+		t.Fatalf("Expected result 0, but got %d", result)
+	}
+}
+
+
+//test for the correct values
+
+func Test_For_The_Correct_Subtraction(t *testing.T){
 	var a int = 20
 	var b int = 10
 
-	result, err := SubTraction(a, b)
-	if err != nil {
-		t.Errorf("Error: %v", err)
-	}
-
-	expected := a - b
-	if result != expected {
-		t.Errorf("Expected result %d, but got %d", expected, result)
+result,err := SubTraction(a,b)
+if err !=nil{
+	t.Fatalf("Expected no error, but got %v",err)
+}
+expectedResult := a-b
+if result != expectedResult{
+	t.Fatalf("Expected result %d, but got %d",expectedResult,result)
 	}
 }
