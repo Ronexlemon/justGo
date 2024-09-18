83d517755a0dca6169c1df796ed8d9456164f1bf
RonexLemon <ronexondimu@gmail.com>
2024-09-18 05:47:52 +0300

    creating slice

--- Slice/slice/slice.go
+++ Slice/slice/slice.go
@@ -24,8 +24,18 @@ var mySlice2  []int =[]int{1,2,3,4,5}
 //Array syntax var myArray =[length]datatype{values}
 //myslice = myArray[start:end]
 var myArray = [5]int{1,2,3,4,5}
-var mySlice3 []int= myArray[0:4]
+var mySlice3 []int= myArray[0:len(myArray)]
+
+//CREATE A SLICE WITH THE MAKE() FUNCTION
+//Syntax slice_name= make([]datatype,length,capacity)
+//NB => if the capacity parameter is not defined, it will be equal to length
+var mySlice4 []int = make([]int, 5, 10)
 
 func Slice(){
 	fmt.Println(cap(mySlice2))
+	fmt.Println(mySlice3)
+	fmt.Println(mySlice4)
+	fmt.Println( len(mySlice4))
+	fmt.Println(cap(mySlice4))
+
 }
